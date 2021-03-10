79eda422ddd57973d17f2dab66ec42c39fba5208
Gary Sims <gary.sims@gmail.com>
2021-03-10 15:13:31 +0200

    First commit

---
+++ build/pico-sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bs2_default.dir/boot2_w25q080.S.obj"
+  "bs2_default.pdb"
+  "bs2_default.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM)
+  include(CMakeFiles/bs2_default.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/pico-sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bs2_default.
+# This may be replaced when dependencies are built.
